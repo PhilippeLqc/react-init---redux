3372b7c1719d56fb87c211f980e28f8a6a950887
Philippe <philippe.lequoc@gmail.com>
2024-02-20 16:23:06 +0100

    add retrait function

--- src/component/Retrait.js
+++ src/component/Retrait.js
@@ -1,45 +1,58 @@
 import { React, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { login, setAmount, setDecouvert, setSold } from "../reducer/user";
-import { TextField, Button, dividerClasses } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setAmount, setDecouvert, setSold } from "../reducer/user";
+import { TextField, Button } from "@mui/material";
 import { Box } from "@mui/system";
 
 function Retrait() {
-  const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const [retrait, setRetrait] = useState({
-    decouvert: "",
-    amount: "",
-    sold: "",
+    decouvert: Number,
+    amount: Number,
+    sold: Number,
   });
 
   const handleInput = (e) => {
     const { name, value } = e.target;
-    setRetrait({ ...retrait, [name]: value });
+    const numericValue = parseFloat(value); // Convertir la valeur en nombre
+    setRetrait({ ...retrait, [name]: numericValue });
     if (name === "decouvert") {
-      dispatch(setDecouvert(value));
+      dispatch(setDecouvert(numericValue));
     } else if (name === "amount") {
-      dispatch(setAmount(value));
+      dispatch(setAmount(numericValue));
     } else if (name === "sold") {
-      dispatch(setSold(value));
+      dispatch(setSold(numericValue));
     }
   };
 
   const handleRetrait = () => {
-    // - Demandez le montant du découvert (0 s'il n'y a pas découvert autorisé) ;
-    // - Demandez le solde en cours ;
-    // - Tant que le solde le permet, demandez au client s'il souhaite effectuer un autre retrait. Il saisira la valeur du retrait ou 0 pour quitter ;
-    // - Affichez le nouveau solde et le montant du découvert ou "solde insuffisant".
-    if (!retrait.sold > retrait.amount) {
+    let newSolde;
+    let { sold, amount, decouvert } = retrait;
+    if (!(sold >= amount)) {
       alert("solde insuffisant");
     } else {
-      const montant = prompt("Entrez le montant du découvert");
-      const solde = prompt("Entrez le solde en cours");
-      const retrait = prompt("Entrez le montant du retrait");
-      const nouveauSolde = solde - retrait;
-      alert(`Nouveau solde : ${nouveauSolde}`);
+      newSolde = sold - amount;
+      alert(`Nouveau solde : ${newSolde}, Découvert restant: ${decouvert}`);
 
-      //utilisez do while pour demander au client s'il souhaite effectuer un autre retrait en prenant en compte le nouveau solde
+      do {
+        const retraitValue = parseFloat(
+          prompt(
+            "Voulez-vous effectuer un autre retrait ? Entrez le montant ou 0 pour quitter"
+          )
+        );
+        if (retraitValue === 0) {
+          break;
+        } else if (retraitValue > newSolde + decouvert) {
+          alert("solde insuffisant");
+        } else {
+          newSolde -= retraitValue;
+          if (newSolde < 0 || decouvert > 0) {
+            decouvert -= Math.abs(newSolde); // Réduire le découvert si le solde devient négatif
+            newSolde = 0; // Le solde ne peut pas être négatif
+          }
+          alert(`Nouveau solde : ${newSolde}, Découvert restant: ${decouvert}`);
+        }
+      } while (newSolde > 0 || decouvert > 0);
     }
   };
 
