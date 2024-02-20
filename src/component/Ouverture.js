e0e133dc84a79387678ba84b6ff9e1835ec87a96
Elise <elise.maaiza33@gmail.com>
2024-02-20 20:52:49 +0100

    just have to fix the overdraftAmount when !isOverdraft

--- src/component/Ouverture.js
+++ src/component/Ouverture.js
@@ -1,23 +1,46 @@
 import { React, useState, useEffect } from "react";
 import "./Ouverture.css";
-import usestate from "usestate";
 
 function Ouverture() {
+	const [isOverdraft, setIsOverdraft] = useState(false);
 	const [isChoiceVisible, setChoiceVisible] = useState(false);
 	const [choice, setChoice] = useState("");
 	const [isOverdraftContainerVisible, setOverdraftContainerVisible] = useState(false);
+	const [overdraftAlert, setOverdraftAlert] = useState(false);
 	const [isAmountContainerVisible, setAmountContainerVisible] = useState(false);
+	const [amountAlert, setAmountAlert] = useState(false);
 	const [isRecapVisible, setRecapVisible] = useState(false);
+	const [overdraftAmount, setOverdraftAmount] = useState("");
+	const [transfer, setTransfer] = useState("");
+
+	const handleInputOverdraft = (e) => {
+		if (e.target.value < 100 || e.target.value > 2000) {
+			setOverdraftAlert(true);
+		} else {
+			setOverdraftAlert(false);
+			setOverdraftAmount(e.target.value);
+		}
+	}
+
+	const handleInputAmount = (e) => {
+		if (e.target.value < 500) {
+			setAmountAlert(true);
+		} else {
+			setAmountAlert(false);
+			setTransfer(e.target.value);
+		}
+	}
 
 	useEffect(() => {
 		const handleKeyDown = (e) => {
-			if (e.key == "y") {
+			if (e.key == "y" && !isOverdraft) {
+				setIsOverdraft(true);
 				setChoiceVisible(true);
 				setChoice("Oui");
 				setOverdraftContainerVisible(true);
 				setAmountContainerVisible(true);
 				setRecapVisible(true);
-			} else {
+			} else if (e.key !== "y" && !isOverdraft) {
 				setChoiceVisible(true);
 				setChoice("Non");
 				setAmountContainerVisible(true);
@@ -30,7 +53,7 @@ function Ouverture() {
 		return () => {
 			document.body.removeEventListener('keydown', handleKeyDown);
 		};
-	}, []);
+	}, [isOverdraft]);
 
   return (
 		<main tabIndex={0}>
@@ -45,23 +68,23 @@ function Ouverture() {
 				{isOverdraftContainerVisible && 
 					<div className="overdraft-container">
 						<label htmlFor="overdraft">Saisissez le montant du découvert entre 100€ et 2000€</label>
-						<input type="number" name="overdraft" id="overdraft" min="100" max="2000" placeholder="100" required />
-						<p id="overdraft-alert">Veuillez saisir un montant compris entre 100€ et 2000€</p>
+						<input type="number" name="overdraft" id="overdraft" min="100" max="2000" placeholder="100" required onChange={handleInputOverdraft} />
+						<p id="overdraft-alert" style={{ display: overdraftAlert ? 'block' : 'none' }}>Veuillez saisir un montant compris entre 100€ et 2000€</p>
 					</div>
 				}
 
 				{isAmountContainerVisible && 
 					<div className="amount-container">
 						<label htmlFor="amount">Saisissez le montant de dépôt (minimum 500€)</label>
-						<input type="number" name="amount" id="amount" min="500" placeholder="500" required />
-						<p id="amount-alert">Veuillez saisir un montant minimum de 500 €</p>
+						<input type="number" name="amount" id="amount" min="500" placeholder="500" required onChange={handleInputAmount} />
+						<p id="amount-alert" style={{ display: amountAlert ? 'block' : 'none' }}>Veuillez saisir un montant minimum de 500 €</p>
 					</div>
 				}
 
 				{isRecapVisible &&
 					<div className="recap-container">
-						<p>Montant du découvert autorisé : <span id="allowed-overdraft"></span>€</p>
-						<p>Montant à transférer : <span id="transfer"></span>€</p>
+						<p>Montant du découvert autorisé : <span id="allowed-overdraft">{overdraftAmount}</span>€</p>
+						<p>Montant à transférer : <span id="transfer">{transfer}</span>€</p>
 					</div>
 				}
 			</div>
